501547f7510ab23da9b7506bf224d6f704a2befc
Kervin Leonardo <sdeclases48@gmail.com>
2024-11-15 22:29:15 -0400

    Primera parte del layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": { width: "0%" },
+          "100%": { width: "329px" },
+        },
+        blink: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        typing: "typing 2s steps(20, end) forwards",
+        blink: "blink 0.5s step-end infinite",
+      },
+      fontFamily: {
+        sans: ["sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        Satisfy: ["Satisfy", "sans-serif"],
+      },
+    },
+  },
+};
